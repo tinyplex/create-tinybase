ffaccdac97be648f75d85a339d8f511570139cc1
James Pearce <github@tripleodeon.com>
2026-01-09 08:57:09 -0800

    Inline returns

--- templates/base/eslint.config.template.js
+++ templates/base/eslint.config.template.js
@@ -1,4 +1,33 @@
 export default [
-  /// return context.isTypescript ? await includeBlock('partials/eslint.template.js', 'typescript-config') : await includeBlock('partials/eslint.template.js', 'javascript-config');
-  /// if (context.isReact) return await includeBlock('partials/eslint.template.js', 'react-config');
+  /// IF context.isTypescript
+  /// addImport("import tseslint from 'typescript-eslint';");
+  ...tseslint.configs.recommended,
+  /// ELSE
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+    },
+  },
+  /// ENDIF
+
+  /// IF context.isReact
+  /// addImport("import pluginReact from 'eslint-plugin-react';");
+  /// addImport("import pluginReactHooks from 'eslint-plugin-react-hooks';");
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  /// ENDIF
 ];
