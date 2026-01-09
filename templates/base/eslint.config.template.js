c05078e468ad7b59e0825a7381aa85da4993555f
James Pearce <github@tripleodeon.com>
2026-01-09 08:17:35 -0800

    Better templates

--- templates/base/eslint.config.template.js
+++ templates/base/eslint.config.template.js
@@ -1,7 +1,4 @@
-/// context.isTypescript && addImport("import tseslint from 'typescript-eslint';");
-/// context.isReact && addImport("import pluginReact from 'eslint-plugin-react';");
-/// context.isReact && addImport("import pluginReactHooks from 'eslint-plugin-react-hooks';");
-/// return 'export default [';
-/// return context.isTypescript ? '  ...tseslint.configs.recommended,' : '  {\n    rules: {\n      \'no-unused-vars\': \'warn\',\n      \'no-undef\': \'error\',\n    },\n  },';
-/// return context.isReact ? '  pluginReact.configs.flat.recommended,\n  {\n    plugins: {\n      \'react-hooks\': pluginReactHooks,\n    },\n    rules: {\n      ...pluginReactHooks.configs.recommended.rules,\n      \'react/react-in-jsx-scope\': \'off\',\n    },\n    settings: {\n      react: {\n        version: \'detect\',\n      },\n    },\n  },' : '';
-/// return '];';
+export default [
+  /// return context.isTypescript ? await includeBlock('partials/eslint.template.js', 'typescript-config') : await includeBlock('partials/eslint.template.js', 'javascript-config');
+  /// if (context.isReact) return await includeBlock('partials/eslint.template.js', 'react-config');
+];
